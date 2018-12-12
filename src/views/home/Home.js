0063186cc9e978d4da7ec5cab8fb21d053804cd9
Thomas Deruel <t.deruel@hotmail.fr>
2018-12-12 21:48:02 +0100

    add simple player buttons

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,85 +1,24 @@
 import React from "react";
-import styled from 'styled-components';
-import Menu from '../../components/menu/Menu';
-import Fonts from '../../styles/fonts';
-import Colors from '../../styles/colors';
-import backgroundImage from '../../assets/img/background_home.jpg';
-
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import InstrumentContainer from '../../views/instrument/InstrumentContainer';
 
 const Home = () => (
-  <HomeComponent>
-    <Header>
-      <Menu/>
-      <div className="title-container">
-        <h2 className="title">Une expérience musicale inédite</h2>
-        <h3 className="subtitle">Découvrez des instruments uniques et amusez vous avec…</h3>
-      </div>
-    </Header>
-  </HomeComponent>
+  <div>
+    <h1>Homepage</h1>
+
+    <li><Link to='/instrument' >
+      instrument
+    </Link></li>
+    <li><Link to='/map' >
+      map
+    </Link></li>
+    <li><Link to='/studio' >
+      studio
+    </Link></li>
+
+  </div>
 );
-
-
-const HomeComponent = styled.div
-  `
-  width: 100%;
-  height: 500vh;
-  background: rgb(13,0,35);
-  background: linear-gradient(194deg, rgba(13,0,35,1) 0%, rgba(53,0,123,1) 26%, rgba(91,9,186,1) 58%, rgba(191,0,210,1) 100%);
-  overflow: hidden;
- 
-
-  `
-
-
-  const Header = styled.div `  
- width: 100%;
-  height: 100vh;
-  background: url(${backgroundImage}) no-repeat;
-  background-size: 100% 100%;
-  .title-container {
-    margin: 30vh auto;
-    text-align: center;
-    width: 100%;
-    height: 6rem;
-    
-    
-  }
-  .title {
-    color: ${Colors.text};
-    font-family: ${Fonts.title};
-    text-transform: uppercase;
-    font-size: 4.58rem;
-    margin-bottom: 3.5rem;
-  }
-  .subtitle {
-    color: ${Colors.text};
-    font-family: ${Fonts.subtitle};
-    font-size: 2.3rem;
-    font-weight: 200;
-  }
-  
-  @media(max-width: 560px) {
-   .header {
-    background-size: cover;
-   }
-   
-    .title {
-    width: 70%;
-   
-    font-size: 2.2rem;
-    margin-bottom: 3.5rem;
-    line-height: 3rem;
-    padding:  0 0 0 4rem;
-  }
-  .subtitle {
-    font-size: 1.5rem;
-    line-height: 2.5rem;
-  }
-  
-  }
-
-`
-;
 
 
 export default Home;
--- src/views/studio/Studio.js
+++ src/views/studio/Studio.js
@@ -1,13 +1,55 @@
 import React from "react";
+import styled from 'styled-components';
+import Colors from '../../styles/colors';
+import Fonts from '../../styles/fonts';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const Studio = () => (
-  <div>
-    <h1>Studio</h1>
+import { Grid, Image, Button } from 'semantic-ui-react';
 
-  </div>
+const Studio = () => (
+  <StudioComponent>
+    <h1>Menu</h1>
+    <h1 id="title">Mélodie</h1>
+    <Grid verticalAlign='middle' centered columns={3}>
+      <Grid.Column textAlign='center' width={3}>
+        <Button className="btn-audio" circular icon='info' size='large'/>
+      </Grid.Column>
+
+      <Grid.Column textAlign='center'width={4}>
+        <Button className="btn-audio" circular icon='play' size='huge'/>
+      </Grid.Column>
+
+      <Grid.Column textAlign='center' width={3} >
+        <Button className="btn-audio" circular icon='volume off' size='large'/>
+      </Grid.Column>
+
+    </Grid>
+
+  </StudioComponent>
 );
 
+const StudioComponent = styled.div
+  `
+  height:100vh;
+  background: ${Colors.primary};
+  background: linear-gradient(21deg, rgba(153,11,223,1) 0%, rgba(152,35,141,1) 100%);
+  
+  #title{
+    text-align:center;
+    font-size: 24px;
+    margin-bottom:16px;
+    color: ${Colors.text};
+    font-family: ${Fonts.title};
+  }
+  .ui.button.btn-audio{
+    transition: all 0.3s ease;
+    background-color:rgba(0,0,0,0.39);
+    color:#FFFCF2;
+  }
+  .ui.button.btn-audio:hover{
+    background-color:rgba(0,0,0,0.8);
+  }
+  `
 
 export default Studio;
