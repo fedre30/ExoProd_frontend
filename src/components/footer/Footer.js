446b48e269196a42f8adb390daf113a630c2972d
Federica Alfano <alfanofederica95@gmail.com>
2018-12-13 15:59:49 +0100

    Footer implemented

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,16 +6,27 @@ import Colors from '../../styles/colors';
 class Footer extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-
-    }
+    this.state = {}
   }
 
 
   render() {
     return (
       <FooterComponent>
+        <div className="logo">© ExoProd</div>
+        <div className="link-container">
+          <ul className="link-list">
+
+              <li className="link"><Link to="/">A propos</Link></li>
+
+
+              <li className="link"><Link to="/">Contact</Link></li>
+
+
+              <li className="link"><Link to="/">Mentions légales</Link></li>
+
+          </ul>
+        </div>
 
       </FooterComponent>
     );
@@ -27,10 +38,52 @@ const FooterComponent = styled.div
   width: 100%;
   height: 3rem;
   display: flex;
-  padding: 2rem 1rem;
+  padding: 2rem 2rem;
   background-color: ${Colors.footer};
   position: absolute;
   bottom: 0;
+  justify-content: space-between;
+  color: ${Colors.text};
+  
+  .logo {
+    font-size: 2rem;
+    
+  
+  }
+  
+  .link-container, .link-list {
+    display: flex;
+   
+  }
+  
+  .link {
+    margin: 0 2rem 0.5rem 0;
+    font-size: 1.3rem;
+   
+   
+  }
+  
+  .link::after{
+    content: '|';
+    display: inline-block;
+    margin-left: 2rem;
+  }
+  
+  .link:last-child::after{
+    display: none;
+  }
+  
+ 
+  a {
+      color: ${Colors.text}
+      transition: color .2s ease;
+    }
+    
+    a:hover {
+      color: ${Colors.fourth}
+    }
+  
+  
   
 
   
