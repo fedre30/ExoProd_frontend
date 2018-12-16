ea702cd8e2c16a3798f6b59e8649726ee221f316
Thomas Deruel <t.deruel@hotmail.fr>
2018-12-16 20:24:11 +0100

    update

--- src/views/studio/Studio.js
+++ src/views/studio/Studio.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import styled from 'styled-components';
 import Colors from '../../styles/colors';
 import Fonts from '../../styles/fonts';
-import AudioPlayer from '../../components/studio/ControlePlayer';
-
+import ovale from '../../assets/img/ovale-dotted.png'
 import protosound from '../../assets/proto-sound/silence-voice.mp3';
 import { Grid, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
@@ -16,14 +15,6 @@ class Studio extends Component {
     super(props);
   }
 
-  state = {
-    isPlaying: false,
-    isMute: false,
-    isResume: true,
-    isStoped: false,
-    playStatus: false,
-  }
-
   render(){
     return (
       <StudioComponent>
@@ -32,8 +23,11 @@ class Studio extends Component {
         <Grid centered columns={3}>
           <Grid.Row columns={1}>
             <Grid.Column >
-              <div className="studio-display-instrument mobile">
-                <p className="studio-display-instrument-instruction">Choisissez un instrument</p>
+              <div className="studio-display-container">
+                <div className="studio-display-instrument mobile">
+                  <p className="studio-display-instrument-instruction">Choisissez un instrument</p>
+                </div>
+                <img style={imageStyle}src={ovale}/>
               </div>
             </Grid.Column>
           </Grid.Row>
@@ -50,7 +44,7 @@ const StudioComponent = styled.div
   `
   height:100vh;
   background: ${Colors.primary};
-  background: linear-gradient(21deg, rgba(153,11,223,1) 0%, rgba(152,35,141,1) 100%);
+  background: linear-gradient(194deg, rgba(13,0,35,1) 0%, rgba(53,0,123,1) 26%, rgba(91,9,186,1) 58%, rgba(191,0,210,1) 100%);
   
   #title {
     text-align:center;
@@ -70,7 +64,15 @@ const StudioComponent = styled.div
   .studio-btn-audio.mobile:nth-child(2){
     margin: 0 24px;
   }
+  .studio-display-container {
+    position:relative;
+    margin-top: 32px;
+    margin-bottom: 12px;
+    width:100%;
+  }
   .studio-display-instrument.mobile {
+    position:relative;
+    z-index:1;
     width: 240px;
     height: 240px;
     border-radius:50%;
@@ -86,5 +88,17 @@ const StudioComponent = styled.div
     font-size: 16px;
   }
   `
-
+const imageStyle ={
+  position:'absolute',
+  zIndex:0,top:'50%',
+  left:'50%',
+  width:'304px',
+  height:'304px',
+  transform:'translate(-50%,-50%)',
+  KhtmlUserSelect: 'none',
+  OUserSelect: 'none',
+  MozUserSelect: 'none',
+  WebkitUserSelect: 'none',
+  userSelect: 'none',
+}
 export default Studio;
