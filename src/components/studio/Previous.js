0d0f90614346e4876d8c68c536b0a39921332d0e
Thomas Deruel <t.deruel@hotmail.fr>
2019-01-16 16:34:33 +0100

    studio end

--- src/components/studio/Previous.js
+++ src/components/studio/Previous.js
@@ -31,7 +31,17 @@ position:absolute;
 z-index: 999999;
 top:calc(50% - 200px);
 left:15vw;
-
+button {
+    outline: 0;
+}
+@media screen and (min-width: 768px){
+    top:calc(3vw + 60px);
+    left:50%;
+    transform: translateX(-50%);
+}
+@media screen and (min-width: 920px){
+    top:5vw;
+}
 `
 const ButtonMobile = styled.button
 `
@@ -44,16 +54,15 @@ border-radius: 50%;
 display:flex;
 align-items:center;
 justify-content: center;
-outline: 0;
 
 img:first-child {
     width:20px;
     height: 20px;
-    opacity:0.4;
+    opacity:0.7;
 }
 
 img:first-child{
-    animation: rotating 3s linear infinite;
+    animation: rotating 4s linear infinite;
 }
 
 
@@ -73,7 +82,8 @@ img:first-child{
       -o-transform: rotate(-360deg);
       transform: rotate(-360deg);
     }
-  }
+}
+
 @media screen and (min-width: 768px){
     display:none;
 }
@@ -83,6 +93,44 @@ const ButtonDesktop = styled.button
 display:none;
 @media screen and (min-width: 768px){
     display:block;
+    text-transform: uppercase;
+    cursor: pointer;
+    font:${Fonts.subtitle};
+    border:0;
+    font-size: 12px;
+    background:0;
+    font-weight:bolder;
+    color:${Colors.text};
+    :after{
+        background-image:url('${arrowDown}');
+        width: 16px;
+        content: " ";
+        position: absolute;
+        right: -16px;
+        height:16px;
+        transform:rotate(180deg);
+        line-height: normal;
+        animation: up 1.5s linear infinite;
+    }
+    @keyframes up{
+        0% {
+         bottom:-12px;
+         opacity: 0;
+        }
+        10%{
+         opacity: 0;
+        }
+        50%{
+        opacity:1;
+        }
+        90% {
+        opacity:0;
+        }
+        100% {
+        bottom: 12px;
+        opacity:0;
+        }
+    }
 }
 `
 export default Previous;
